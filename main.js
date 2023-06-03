8a22977e7298c67bd51828414a79334ad1b74c27
sayuri <tobisayu0228@gmail.com>
2023-06-03 17:08:49 +0900

    変更1

---
+++ main.js
@@ -0,0 +1,31 @@
+if (window.matchMedia('(max-width: 375px)').matches) {
+    //スマホ処理
+
+    // ハンバーガーメニュー
+} else {
+    //PC処理
+
+    // Serviceの文章のところ
+    // スクロールした時にふわっと表示する
+    $(window).scroll(function () {
+        const scrollAnimationElm = document.querySelectorAll('.scroll_up');
+        let scrollAnimationFunc = function () {
+          for (let i = 0; i < scrollAnimationElm.length; i++) {
+            let triggerMargin = 100;
+            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+              scrollAnimationElm[i].classList.add('on');
+            }
+          }
+        }
+        window.addEventListener('load', scrollAnimationFunc);
+        window.addEventListener('scroll', scrollAnimationFunc);
+      });
+    // スクロールトップ
+    const scrollTop = document.getElementById("scroll-top");
+    scrollTop.addEventListener("click", () => {
+      window.scrollTo ({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+}
