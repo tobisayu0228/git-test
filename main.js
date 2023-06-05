0c963e302c70719579603dcca6b7ad66f7f768d9
sayuri <tobisayu0228@gmail.com>
2023-06-05 12:38:43 +0900

    変更5

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
 if (window.matchMedia('(max-width: 375px)').matches) {
     //スマホ処理
+
+    // ハンバーガーメニュー
     $(".openbtn").click(function () {
       $(this).toggleClass('active');
   });
-
-    // ハンバーガーメニュー
+  // スクロールトップ
+  const scrollTop = document.getElementById("scroll-top");
+  scrollTop.addEventListener("click", () => {
+    window.scrollTo ({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
 } else {
     //PC処理
 
     // Serviceの文章のところ
     // スクロールした時にふわっと表示する
-    $(window).scroll(function () {
-        const scrollAnimationElm = document.querySelectorAll('.scroll_up');
-        let scrollAnimationFunc = function () {
-          for (let i = 0; i < scrollAnimationElm.length; i++) {
-            let triggerMargin = 100;
-            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-              scrollAnimationElm[i].classList.add('on');
-            }
+  $(window).scroll(function () {
+      const scrollAnimationElm = document.querySelectorAll('.scroll_up');
+      let scrollAnimationFunc = function () {
+        for (let i = 0; i < scrollAnimationElm.length; i++) {
+          let triggerMargin = 100;
+          if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+            scrollAnimationElm[i].classList.add('on');
           }
         }
-        window.addEventListener('load', scrollAnimationFunc);
-        window.addEventListener('scroll', scrollAnimationFunc);
-      });
-    // スクロールトップ
-    const scrollTop = document.getElementById("scroll-top");
-    scrollTop.addEventListener("click", () => {
-      window.scrollTo ({
-        top: 0,
-        behavior: "smooth"
-      });
+      }
+      window.addEventListener('load', scrollAnimationFunc);
+      window.addEventListener('scroll', scrollAnimationFunc);
     });
+  // スクロールトップ
+  const scrollTop = document.getElementById("scroll-top");
+  scrollTop.addEventListener("click", () => {
+    window.scrollTo ({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
 }
